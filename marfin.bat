433e95cd63ea9d2b574bfa7856aa6dbd6bf67b12
linof007 <hugolinof@gmail.com>
2026-06-17 17:23:28 +0200

    Add files via upload

---
+++ marfin.bat
@@ -0,0 +1,8 @@
+﻿
+
+Rem  marfin.bat
+@Echo off
+cls
+
+start java --module-path %PATH_TO_FX% --add-modules javafx.controls -jar C:\apache-tomcat-9.0.97\webapps\MarWeb\WEB-INF\lib\marfin.jar
+
---
+++ stage1.bat
@@ -0,0 +1,11 @@
+﻿
+@Rem  stage1.bat
+@Echo off
+cls
+
+
+ REM  ### ...".bat-File"Starten, wo .bat-file und .jar-file in gleichen Ordner sich befinden.  ... ###
+
+  start java --module-path %PATH_TO_FX% --add-modules javafx.controls -jar C:\apache-tomcat-9.0.97\webapps\MarWeb\WEB-INF\lib\stage1.jar
+
+  
